6831321604d09c7ed429c7b4b943296788f0eec1
uzair-ur-rahim <rahim_uzair@yahoo.com>
2017-04-06 04:43:59 -0700

    lab

---
+++ app.js
@@ -0,0 +1,28 @@
+// #1
+// for (var i = 1; i <= 5; i++) {
+//     document.write("Hello World" + "</br>");
+// }
+// // #2
+// for (var j = 1; j <= 10; j++) {
+//     document.write(j + "</br>");
+// }
+// // // #3
+// // var table = +prompt ("Enter the desired table","Eg 2,3,4,5,6" );
+// // var number = +prompt ("Tell us the length");
+// // for (var k = 1; k <=number; k++){
+// //     document.write(table + "x" + k + "=" + table * k + "</br>" );
+// // }
+// // #4
+// // var arr = ["Nokia", "Samsung","Apple","Sony","Huawei"];
+// // for (var i=0; i<arr.length; i++ ){
+// //     document.write(arr[i] + "</br>");
+// // }
+// // #5   
+// var friuts = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var m = 0; m < friuts.length; m++){
+//     document.write(friuts[m] + "</br>" )
+    
+// }
+// for (var m = 0; m <friuts.length; m++ ){
+//     document.write("Element at index " + m + " is " + friuts[m] + "</br>")
+// }
